2fb84bcd5b08f70ee45d9579431d242783a9263d
Florian Mittlböck <florian.mittlboeck@web.de>
2016-09-30 22:05:05 +0200

    First commit

---
+++ app/src/test/java/at/htl_grieskirchen/diplomarbeit/koegelbauer_mittlboeck_wiesinger/wander_app_ampflwang/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package at.htl_grieskirchen.diplomarbeit.koegelbauer_mittlboeck_wiesinger.wander_app_ampflwang;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
